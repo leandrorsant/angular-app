cd8c5cf1954cb267beee554c9e9f2e3a270d2b6a
Leandro <ramosdossantos.leandro@gmail.com>
2023-12-11 21:16:09 -0300

    Route fix

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -11,11 +11,12 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   template: `
-    <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-    </section>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+  </section>
   `,
   styleUrls: ['./housing-location.component.css'],
 })
